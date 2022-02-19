e340886276b458486d17293bda408a390de209bf
Saurabh Mahambrey <mahambrey.saurabh07@gmail.com>
2022-02-19 16:53:19 +0530

    commited some old code and added python inorder traversal

--- python/src/data_structures/arrays/tests/test_array_solutions.py
+++ python/src/data_structures/arrays/tests/test_array_solutions.py
@@ -2,12 +2,11 @@ import pytest
 from src.data_structures.arrays.programs.miscellaneous_2 import check_if_unique
 
 
-@pytest.mark.array_tests
+@pytest.mark.array_tests_ps2
 def test_check_if_unique_test_1():
     assert check_if_unique("aabbccd") == False
 
 
-@pytest.mark.array_tests
+@pytest.mark.array_tests_ps2
 def test_check_if_unique_test_2():
     assert check_if_unique("abcedf") == True
-git
---
+++ python/src/data_structures/binary_trees/tree_traversals.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return str(self.data)
+
+    def __str__(self):
+        return str(self.data)
+
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n3.left = n6
+n3.right = n7
+
+"""
+(a) Inorder (Left, Root, Right) : 4 2 5 1 3 
+(b) Preorder (Root, Left, Right) : 1 2 4 5 3 
+(c) Postorder (Left, Right, Root) : 4 5 2 3 1
+"""
+
+
+def recursive_print(n: Node):
+    if n:
+        recursive_print(n.left)
+        print(n)
+        recursive_print(n.right)
+
+
+recursive_print(n1)
