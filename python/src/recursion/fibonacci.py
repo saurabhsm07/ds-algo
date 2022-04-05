04915862d3d161a9f93a3361071aa5f4d5a1170f
maverick <mahambrey.saurabh07@gmail.com>
2022-04-05 20:37:37 +0530

    fixed code issues

--- python/src/recursion/fibonacci.py
+++ python/src/recursion/fibonacci.py
@@ -1,17 +1,13 @@
 from utils.function_timers import get_execution_time_simple
-
-
 class Solution(object):
     records = {1: 1, 0: 0}
 
-    # @get_execution_time_simple
     def fib(self, n):
         if n < 2:
             return n
         else:
             return self.fib(n - 1) + self.fib(n - 2)
 
-    # @get_execution_time_simple
     def fib_memoization(self, n):
         """
         :type n: int
@@ -23,13 +19,19 @@ class Solution(object):
             if n in Solution.records.keys():
                 return Solution.records[n]
             else:
-                Solution.records[n] = self.fib(n - 1) + self.fib(n - 2)
+                Solution.records[n] = self.fib_memoization(n - 1) + self.fib_memoization(n - 2)
                 return Solution.records[n]
 
 
 if __name__ == "__main__":
     import time
 
+    t1 = time.time()
     Solution().fib(30)
+    t2 = time.time()
+    print(t2 - t1)
 
-    Solution().fib_memoization(30)
+    t1 = time.time()
+    Solution().fib_memoization(700)
+    t2 = time.time()
+    print(t2 - t1)
