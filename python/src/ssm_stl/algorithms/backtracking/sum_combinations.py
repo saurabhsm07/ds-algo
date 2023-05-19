0266a6d40f0ece32eda2d09c4d6de82d5b0f52c0
maverick <mahambrey.saurabh07@gmail.com>
2023-05-19 20:44:45 +0530

    working with chat GPT to find solution to this problem

---
+++ python/src/ssm_stl/algorithms/backtracking/sum_combinations.py
@@ -0,0 +1,67 @@
+"""
+Problem Statement:
+Given an integer N and another integer K,
+write a function to find all the unique lists that consist of a combination of numbers from 1 to K,
+where the sum of the elements in each list is equal to N. Each number from 1 to K can be repeated multiple times in a list.
+
+Write a function named find_lists that takes two integers,
+N and K, as input and returns a list of all unique lists that satisfy the given conditions.
+
+Function Signature:
+
+def find_lists(N: int, K: int) -> List[List[int]]:
+
+Examples:
+
+print(find_lists(5, 4))
+# Output: [[1, 1, 1, 1, 1],
+            [1, 1, 1, 2],
+            [1, 1, 2, 1],
+            [1, 1, 3],
+            [1, 2, 1, 1],
+            [1, 2, 2],
+            [1, 3, 1],
+            [1, 4],
+            [2, 1, 1, 1],
+            [2, 1, 2],
+            [2, 2, 1],
+            [2, 3],
+            [3, 1, 1],
+            [3, 2],
+            [4, 1],
+            [5]]
+
+print(find_lists(4, 2))
+# Output: [[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2]]
+
+print(find_lists(6, 3))
+# Output: [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 2], [1, 1, 1, 2, 1], [1, 1, 1, 3], [1, 1, 2, 1, 1], [1, 1, 2, 2], [1, 1, 3, 1], [1, 1, 4], [1, 2, 1, 1, 1], [1, 2, 1, 2], [1, 2, 2, 1], [1, 2, 3], [1, 3, 1, 1], [1, 3, 2], [1, 4, 1], [1, 5], [2, 1, 1, 1, 1], [2, 1, 1, 2], [2, 1, 2, 1], [2, 1, 3], [2, 2, 1, 1], [2, 2, 2], [2, 3, 1], [2, 4], [3, 1, 1, 1], [3, 1, 2], [3, 2, 1], [3, 3], [4, 1, 1], [4, 2], [5, 1], [6]]
+
+Note:
+The order of the lists and the elements within each list does not matter.
+Each list in the output should be unique, i.e., no two lists should have the same elements.
+
+"""
+from typing import List
+
+
+def find_lists(n: int, k: int) -> List[List[int]]:
+    result_list = []
+
+    def backtrack_sum(temp_list, n, k):
+        if n == 0:
+            result_list.append(temp_list)
+            return
+        elif n < 0:
+            return
+        else:
+            for i in range(1, k + 1):
+                n = n - i
+                temp_list = temp_list+[i]
+                backtrack_sum(temp_list, n, k)
+
+    backtrack_sum([], n, k)
+    return result_list
+
+
+print(find_lists(5, 4))
